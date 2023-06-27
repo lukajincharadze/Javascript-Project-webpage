73e952c2c9e5983942e4d09bffe1a72f83e76372
Luka Jincharadze <lukajinchara@gmail.com>
2023-06-27 13:14:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
-  }
+}
 
   // Function to validate the password
   function validatePassword(password) {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]{8,}$/;
-    return passwordRegex.test(password);
+    // Password should contain at least one lowercase letter, one uppercase letter, one digit, and one special character
+    const lowercaseRegex = /[a-z]/;
+    const uppercaseRegex = /[A-Z]/;
+    const digitRegex = /\d/;
+    return (
+      lowercaseRegex.test(password) &&
+      uppercaseRegex.test(password) &&
+      digitRegex.test(password)
+    );
   }
 
   document.getElementById("registration").addEventListener("submit", function(event) {
@@ -33,7 +40,9 @@ function validateEmail(email) {
     if (password.length < 8) {
       errors.password = "Password should be at least 8 characters long";
     } else if (!validatePassword(password)) {
-      errors.password = "Password should contain at least one lowercase letter, one uppercase letter, one digit, and one special character";
+      errors.password = "at least one lowercase letter, uppercase, digit";
+    } else if (validatePassword(password)) {
+        errors.password = "";
     }
 
     if (password !== password2) {
@@ -69,25 +78,39 @@ function validateEmail(email) {
     }
   }
 
+
   togglePasswordVisibility();
 
-  // Clear the email error message when the user starts typing
-  let usernameInput = document.getElementById('user');
-  let errorUsername = document.getElementById('error_username');
+// Clear the email error message when the user starts typing
+let usernameInput = document.getElementById('user');
+let errorUsername = document.getElementById('error_username');
+let passwordInput = document.getElementById('mypassword');
+let passwordInput2 = document.getElementById('mypassword2');
+let errorPassword = document.getElementById('error_password');
+let errorPassword2 = document.getElementById('error_password2');
 
-  usernameInput.addEventListener('input', function() {
+usernameInput.addEventListener('input', function() {
     errorUsername.textContent = '';
-  });
+});
 
-  // BURGER MENU
-  let navigation2 = document.querySelector(".navigation2");
-  let navigation = document.querySelector(".navigation");
-  let navblog = document.querySelector(".nav_blog");
-  let nav_logo = document.querySelector(".nav_logo");
-  let navigationbg = document.querySelector(".navigation2bg");
-  let burger = document.querySelector(".container");
-  let nav_wraper = document.querySelector(".nav_wraper");
-  let circle_img = document.querySelector(".circle_div2");
+passwordInput.addEventListener('input', function() {
+    errorPassword.textContent = '';
+});
+
+passwordInput2.addEventListener('input', function() {
+    errorPassword2.textContent = '';
+});
+
+
+// BURGER MENU
+let navigation2 = document.querySelector(".navigation2");
+let navigation = document.querySelector(".navigation");
+let navblog = document.querySelector(".nav_blog");
+let nav_logo = document.querySelector(".nav_logo");
+let navigationbg = document.querySelector(".navigation2bg");
+let burger = document.querySelector(".container");
+let nav_wraper = document.querySelector(".nav_wraper");
+let circle_img = document.querySelector(".circle_div2");
 
 burger.addEventListener("click", function() {
     circle_img.classList.toggle("active");
