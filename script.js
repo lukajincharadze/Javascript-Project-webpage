7537f094e276fa5f5325196656ac3dfdcca81149
Luka Jincharadze <lukajinchara@gmail.com>
2023-06-28 12:04:59 +0400

    validation correction

--- script.js
+++ script.js
@@ -99,6 +99,23 @@ function togglePasswordVisibility() {
   passwordInput2.addEventListener('input', function() {
     errorPassword2.textContent = '';
   });
+
+
+
+
+  
+  let newsletter_input = document.getElementById('newsletter_input');
+  let newsletter_send = document.getElementById('newsletter_send');
+  
+  newsletter_send.addEventListener('click', function(event) {
+    if (newsletter_input.value !== '') {
+      event.target.closest('form').submit();
+      console.log('Value is not empty');
+    } else {
+      event.preventDefault();
+      console.log('Value is empty');
+    }
+  });
   
 
 // BURGER MENU
