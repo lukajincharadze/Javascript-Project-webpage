8bcd80627044b8469167f52453fe880b458a0a7c
Luka Jincharadze <lukajinchara@gmail.com>
2023-07-03 19:52:27 +0400

    update

---
+++ Javascript/burg_dropdown.js
@@ -0,0 +1,14 @@
+let dropdown_content2 = document.getElementsByClassName('dropdown_content2_a');
+
+Array.from(dropdown_content2).forEach(element => {
+    element.addEventListener('click', () => {
+        circle_img.classList.remove("active");
+        navigation2.classList.remove("active");
+        navblog.classList.remove("active");
+        nav_logo.classList.remove("active");
+        navigation.classList.remove("active");
+        navigationbg.classList.remove("active");
+        nav_wraper.classList.remove("active");
+        burger.classList.remove("change");
+    });
+});
